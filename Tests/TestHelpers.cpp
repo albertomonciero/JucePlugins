1cea576cf564a4281493a2c563e7607acfb775d4
albertomonciero <monciero.alberto@gmail.com>
2023-02-15 19:00:30 +0100

    add getMagnitudeResponse test

--- Tests/TestHelpers.cpp
+++ Tests/TestHelpers.cpp
@@ -35,4 +35,20 @@ namespace TestHelpers
                 CHECK(complexBuffer[ch][i].imag() == 0.0);
             }
     }
+
+    TEST_CASE ("Test that getMagnitudeResponse returns correct values")
+    {
+        ComplexBuffer complexBuffer  { { { 1.0, 1.0 }, { 2.0, 2.0 } },
+                                       { { 3.0, 3.0 }, { 4.0, 4.0 } } };
+ 
+        juce::AudioBuffer<float> expected(complexBuffer.size(), complexBuffer[0].size() / 2);
+        expected.setSample(0, 0, 1.4142135623730951);
+        expected.setSample(1, 0, 4.242640687119285);
+
+        juce::AudioBuffer<float> actual = getMagnitudeResponse(complexBuffer); 
+
+        for (int ch = 0; ch < actual.getNumChannels(); ch++)
+            for (int i = 0; i < actual.getNumSamples(); i++)
+                CHECK(actual.getSample(ch, i) == expected.getSample(ch, i));
+    }
 }
