f3cd10d065479e23ad8457df496e13cbe9d7bff9
albertomonciero <monciero.alberto@gmail.com>
2023-04-15 18:59:28 +0200

    implement OnePoleFilter separated tests for sample rate and cutoff frequency

--- Tests/OnePoleFilterTests.cpp
+++ Tests/OnePoleFilterTests.cpp
@@ -2,60 +2,154 @@
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <shared_modules/shared_modules.h>
 
-TEST_CASE ("Test that one pole lowpass filter has correct frequency response")
+TEST_CASE ("One pole lowpass filter has correct frequency response for different sample rates", "[OnePoleFilter]")
 {
-    float fs = GENERATE(44100, 48000, 96000);
-
-    juce::dsp::ProcessSpec spec {fs, 256, 2};
-
-    size_t fftSize = spec.maximumBlockSize;
-
-    // generate frequency at exact fft bin positions
-    size_t index = static_cast<size_t> (GENERATE(5, 13, 17));
-    float fc = index * spec.sampleRate / static_cast<float> (fftSize);
-
     OnePoleFilter::Lowpass lowpassFilter;
-    lowpassFilter.prepare(spec);
-    lowpassFilter.setCutoffFrequency(fc);
 
-    juce::AudioBuffer<float> impulseResponse = TestHelpers::impulseResponseGenerator(lowpassFilter, spec.numChannels, spec.maximumBlockSize);
-    TestHelpers::ComplexBuffer frequencyResponse = TestHelpers::getFrequencyResponse(impulseResponse, fftSize);
-    juce::AudioBuffer<float> magnitudeResponse = TestHelpers::getMagnitudeResponse(frequencyResponse);
-
-    // check result per channel for DC, fc, and last possible index
-    for (size_t ch = 0; ch < spec.numChannels; ch++)
+    const auto runTest = [&] (const float fs)
     {
-        REQUIRE_THAT(juce::Decibels::gainToDecibels<float>(magnitudeResponse.getSample(ch, 0)), Catch::Matchers::WithinAbs(0.0, 1e-8));
-        REQUIRE_THAT(juce::Decibels::gainToDecibels<float>(magnitudeResponse.getSample(ch, index)), Catch::Matchers::WithinAbs(-3.0, 1e-1));
-        REQUIRE(juce::Decibels::gainToDecibels<float>(magnitudeResponse.getSample(ch, fftSize / 2 - 1)) < -13.0f);
-    }
+        juce::uint32 blockSize = 256;
+        juce::uint32 numChannels = 2;
+
+        juce::dsp::ProcessSpec spec{ fs, blockSize, numChannels };
+
+        size_t fftSize = spec.maximumBlockSize;
+
+        size_t frequencyIndex = 13;
+        float fc = frequencyIndex * spec.sampleRate / static_cast<float> (fftSize);
+
+        lowpassFilter.prepare (spec);
+        lowpassFilter.setCutoffFrequency (fc, true);
+
+        juce::AudioBuffer<float> impulseResponse = TestHelpers::impulseResponseGenerator (lowpassFilter, spec.numChannels, spec.maximumBlockSize);
+        TestHelpers::ComplexBuffer frequencyResponse = TestHelpers::getFrequencyResponse (impulseResponse, fftSize);
+        juce::AudioBuffer<float> magnitudeResponse = TestHelpers::getMagnitudeResponse (frequencyResponse);
+
+        // check result per channel for DC, fc, and last possible index
+        for (size_t ch = 0; ch < spec.numChannels; ch++)
+        {
+            CHECK_THAT (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, 0)), Catch::Matchers::WithinAbs (0.0, 1e-8));
+            CHECK_THAT (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, frequencyIndex)), Catch::Matchers::WithinAbs (-3.0, 1e-1));
+            CHECK (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, fftSize / 2 - 1)) < -13.0f);
+        }
+    };
+
+    std::vector<float> sampleRates{ 44100, 48000, 96000, 192000 };
+
+    for (auto& s : sampleRates)
+        runTest (s);
 }
 
-TEST_CASE ("Test that one pole highpass filter has correct frequency response")
+TEST_CASE ("One pole lowpass filter has correct frequency response for different cutoff frequencies", "[OnePoleFilter]")
 {
-    float fs = GENERATE(44100, 48000, 96000);
+    OnePoleFilter::Lowpass lowpassFilter;
 
-    juce::dsp::ProcessSpec spec {fs, 256, 2};
+    float fs = 48000.0;
+    juce::uint32 blockSize = 256;
+    juce::uint32 numChannels = 2;
+
+    juce::dsp::ProcessSpec spec{ fs, blockSize, numChannels };
+    lowpassFilter.prepare (spec);
 
     size_t fftSize = spec.maximumBlockSize;
 
-    // generate frequency at exact fft bin positions
-    size_t index = static_cast<size_t>(GENERATE(8, 14, 20));
-    float fc = index * spec.sampleRate / static_cast<float> (fftSize);
-
-    OnePoleFilter::Highpass highpassFilter;
-    highpassFilter.prepare(spec);
-    highpassFilter.setCutoffFrequency(fc);
-
-    juce::AudioBuffer<float> impulseResponse = TestHelpers::impulseResponseGenerator(highpassFilter, spec.numChannels, spec.maximumBlockSize);
-    TestHelpers::ComplexBuffer frequencyResponse = TestHelpers::getFrequencyResponse(impulseResponse, fftSize);
-    juce::AudioBuffer<float> magnitudeResponse = TestHelpers::getMagnitudeResponse(frequencyResponse);
-
-    // check result per channel for DC, fc, and last possible index
-    for (size_t ch = 0; ch < spec.numChannels; ch++)
+    const auto runTest = [&] (const size_t frequencyIndex)
     {
-        REQUIRE(juce::Decibels::gainToDecibels<float>(magnitudeResponse.getSample(ch, 0)) < -90.0f);
-        REQUIRE_THAT(juce::Decibels::gainToDecibels<float>(magnitudeResponse.getSample(ch, index)), Catch::Matchers::WithinRel(-3.0, 1e-1));
-        REQUIRE_THAT(juce::Decibels::gainToDecibels<float>(magnitudeResponse.getSample(ch, fftSize / 2 -1)), Catch::Matchers::WithinAbs(0.0, 1e-2));
-    }
+        float fc = frequencyIndex * spec.sampleRate / static_cast<float> (fftSize);
+
+        lowpassFilter.setCutoffFrequency (fc, true);
+
+        juce::AudioBuffer<float> impulseResponse = TestHelpers::impulseResponseGenerator (lowpassFilter, spec.numChannels, spec.maximumBlockSize);
+        TestHelpers::ComplexBuffer frequencyResponse = TestHelpers::getFrequencyResponse (impulseResponse, fftSize);
+        juce::AudioBuffer<float> magnitudeResponse = TestHelpers::getMagnitudeResponse (frequencyResponse);
+
+        // check result per channel for DC, fc, and last possible index
+        for (size_t ch = 0; ch < spec.numChannels; ch++)
+        {
+            CHECK_THAT (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, 0)), Catch::Matchers::WithinAbs (0.0, 1e-8));
+            CHECK_THAT (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, frequencyIndex)), Catch::Matchers::WithinAbs (-3.0, 1e-1));
+            CHECK (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, fftSize / 2 - 1)) < -13.0f);
+        }
+    };
+
+    std::vector<size_t> frequencyIndices{ 5, 10, 13, 17 };
+
+    for (auto& f : frequencyIndices)
+        runTest (f);
+}
+
+TEST_CASE ("One pole highpass filter has correct frequency response for different sample rates", "[OnePoleFilter]")
+{
+    OnePoleFilter::Highpass highpassFilter;
+
+    const auto runTest = [&] (const float fs)
+    {
+        juce::uint32 blockSize = 256;
+        juce::uint32 numChannels = 2;
+
+        juce::dsp::ProcessSpec spec{ fs, blockSize, numChannels };
+
+        size_t fftSize = spec.maximumBlockSize;
+
+        size_t frequencyIndex = 13;
+        float fc = frequencyIndex * spec.sampleRate / static_cast<float> (fftSize);
+
+        highpassFilter.prepare (spec);
+        highpassFilter.setCutoffFrequency (fc, true);
+
+        juce::AudioBuffer<float> impulseResponse = TestHelpers::impulseResponseGenerator (highpassFilter, spec.numChannels, spec.maximumBlockSize);
+        TestHelpers::ComplexBuffer frequencyResponse = TestHelpers::getFrequencyResponse (impulseResponse, fftSize);
+        juce::AudioBuffer<float> magnitudeResponse = TestHelpers::getMagnitudeResponse (frequencyResponse);
+
+        // check result per channel for DC, fc, and last possible index
+        for (size_t ch = 0; ch < spec.numChannels; ch++)
+        {
+            CHECK (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, 0)) < -90.0f);
+            CHECK_THAT (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, frequencyIndex)), Catch::Matchers::WithinRel (-3.0, 1e-1));
+            CHECK_THAT (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, fftSize / 2 - 1)), Catch::Matchers::WithinAbs (0.0, 1e-2));
+        }
+    };
+
+    std::vector<float> sampleRates{ 44100, 48000, 96000, 192000 };
+
+    for (auto& s : sampleRates)
+        runTest (s);
+}
+
+TEST_CASE ("One pole highpass filter has correct frequency response for different cutoff frequencies", "[OnePoleFilter]")
+{
+    OnePoleFilter::Highpass highpassFilter;
+
+    float fs = 48000.0;
+    juce::uint32 blockSize = 256;
+    juce::uint32 numChannels = 2;
+
+    juce::dsp::ProcessSpec spec{ fs, blockSize, numChannels };
+    highpassFilter.prepare (spec);
+
+    size_t fftSize = spec.maximumBlockSize;
+
+    const auto runTest = [&] (const size_t frequencyIndex)
+    {
+        float fc = frequencyIndex * spec.sampleRate / static_cast<float> (fftSize);
+
+        highpassFilter.setCutoffFrequency (fc, true);
+
+        juce::AudioBuffer<float> impulseResponse = TestHelpers::impulseResponseGenerator (highpassFilter, spec.numChannels, spec.maximumBlockSize);
+        TestHelpers::ComplexBuffer frequencyResponse = TestHelpers::getFrequencyResponse (impulseResponse, fftSize);
+        juce::AudioBuffer<float> magnitudeResponse = TestHelpers::getMagnitudeResponse (frequencyResponse);
+
+        // check result per channel for DC, fc, and last possible index
+        for (size_t ch = 0; ch < spec.numChannels; ch++)
+        {
+            CHECK (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, 0)) < -90.0f);
+            CHECK_THAT (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, frequencyIndex)), Catch::Matchers::WithinRel (-3.0, 1e-1));
+            CHECK_THAT (juce::Decibels::gainToDecibels<float> (magnitudeResponse.getSample (ch, fftSize / 2 - 1)), Catch::Matchers::WithinAbs (0.0, 1e-2));
+        }
+    };
+
+    std::vector<size_t> frequencyIndices{ 8, 14, 20, 22 };
+
+    for (auto& f : frequencyIndices)
+        runTest (f);
 }
